36607935f25f52bd1c799440bf719cb6c8f37474
Henry Pérez <dwverling@gmail.com>
2022-08-17 02:58:15 -0500

    Mejorando el código fuente y la interfaz de usuario

--- CornDiseaseDetection/app/src/main/java/com/example/corndiseasedetection/MainActivity.java
+++ CornDiseaseDetection/app/src/main/java/com/example/corndiseasedetection/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.corndiseasedetection;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.os.Bundle;
@@ -15,7 +23,6 @@ import android.widget.TextView;
 
 import com.example.corndiseasedetection.ml.Model;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
@@ -31,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private Button captureImageBtn, detectDiseaseBtn;
     private ImageView imageView;
     private TextView textResult;
-    Bitmap imageBitmap;
+    private Bitmap imageBitmap;
+
+    private ActivityResultLauncher<Intent> mStartForResult;
 
     int imageSize = 224;
 
@@ -46,58 +55,33 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_view);
         textResult = findViewById(R.id.text_result0);
 
-        captureImageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Para capturar la imagen
-                dispatchTakePictureIntent();
-            }
-        });
+        captureImageBtn.setOnClickListener(btnCapturarImg);
+        detectDiseaseBtn.setOnClickListener(btnDetectarEnf);
 
-        detectDiseaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Para detectar la enfermedad
-            }
-        });
-
+        this.registrarActivityResult();
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    private View.OnClickListener btnCapturarImg = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mStartForResult.launch(intent);
         }
-    }
+    };
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-
-            int dimension = Math.min(imageBitmap.getWidth(), imageBitmap.getHeight());
-            imageBitmap = ThumbnailUtils.extractThumbnail(imageBitmap, dimension, dimension);
-
-            imageView.setImageBitmap(imageBitmap);
-
-            imageBitmap = Bitmap.createScaledBitmap(imageBitmap, imageSize, imageSize,false);
+    private View.OnClickListener btnDetectarEnf = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
             classifyImage(imageBitmap);
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+    };
 
     public void classifyImage(Bitmap image){
         try {
             Model model = Model.newInstance(getApplicationContext());
 
             // Creates inputs for reference.
-
-            // Creates inputs for reference.
             //inputFeature0.loadBuffer(byteBuffer);
-
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
             ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4* imageSize* imageSize * 3);
             byteBuffer.order(ByteOrder.nativeOrder());
@@ -108,11 +92,6 @@ public class MainActivity extends AppCompatActivity {
             for(int i = 0; i < imageSize; i++){
                 for(int j = 0; j < imageSize; j++){
                     int val = intValues[pixel++];
-                   /*
-                    byteBuffer.putFloat(((val >> 16) & 0xFF)*(1.f/255.f));
-                    byteBuffer.putFloat(((val >> 8) & 0xFF)*(1.f/255.f));
-                    byteBuffer.putFloat((val & 0xFF)*(1.f/255.f));
-                    */
                     byteBuffer.putFloat(((val >> 16) & 0xFF)*(1.f/1));
                     byteBuffer.putFloat(((val >> 8) & 0xFF)*(1.f/1));
                     byteBuffer.putFloat((val & 0xFF)*(1.f/1));
@@ -142,20 +121,15 @@ public class MainActivity extends AppCompatActivity {
                     classes[3]+" : " + formatear(confidences[3])+"%");
 
             //txtalpha2Code = classes[maxPos];
-
             String s = "";
             for(int i = 0; i < classes.length; i++){
                 s += String.format("%s: %.1f%%\n", classes[i], confidences[i] *100);
 
             }
             ///confidence.setText(s);
-
-
             // Releases model resources if no longer used.
             model.close();
-        } catch (IOException e) {
-            // TODO Handle the exception
-        }
+        } catch (IOException e) {}
     }
 
     private String formatear(float valor){
@@ -165,6 +139,22 @@ public class MainActivity extends AppCompatActivity {
         return format.format(valor);
     }
 
+    private void registrarActivityResult(){
+        mStartForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Bundle extras = result.getData().getExtras();
+                        imageBitmap = (Bitmap) extras.get("data");
 
-
+                        int dimension = Math.min(imageBitmap.getWidth(), imageBitmap.getHeight());
+                        imageBitmap = ThumbnailUtils.extractThumbnail(imageBitmap, dimension, dimension);
+                        imageView.setImageBitmap(imageBitmap);
+                        imageBitmap = Bitmap.createScaledBitmap(imageBitmap, imageSize, imageSize,false);
+                        //Luego de esto, el btn Detectar enfermedad se encarga de clasificar
+                    }
+                }
+        );
+    }
 }
---
+++ CornDiseaseDetection/app/src/main/java/com/example/corndiseasedetection/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.corndiseasedetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
